7f56292c63ab16a0146f7d4ddc36dfc24b7d19d0
yinzhu5 <80451135+yinzhu5@users.noreply.github.com>
2023-04-01 02:03:54 +1100

    process gathering information and output

--- notebook/new_soln_draft.py
+++ notebook/new_soln_draft.py
@@ -76,23 +76,20 @@ def update_stats(tweet_location, author_id):
             return 
         
 
-    # update user_stats and gcc_stats
+    # update user_stats
     if author_id not in user_stats:
-        user_stats[author_id] = [1, {gcc:1}, 1] # tweet count, tweet count from each gcc, diff gccs
+        user_stats[author_id] = {gcc:1}
     else:
-        user_stats[author_id][0] += 1 # add 1 to tweet count
+        if gcc not in user_stats[author_id]:
+            user_stats[author_id][gcc] = 1 # add gcc to tally and record 1 tweet
+        else:
+            user_stats[author_id][gcc] += 1 # add tweet count to gcc tally
 
-    if gcc not in user_stats[author_id][1]:
-        user_stats[author_id][1][gcc] = 1 # add gcc to tally
-
-        user_stats[author_id][2] += 1 # add to diff gcc tally
-    else:
-        user_stats[author_id][1][gcc] += 1 # add tweet count to gcc tally
-    
+    # update gcc_stats
     if gcc not in gcc_stats:
-        gcc_stats[f'{gcc} ({task2_map[gcc]})'] = 1 # add gcc to tally
+        gcc_stats[gcc] = 1 # add gcc to tally
     else:
-        gcc_stats[f'{gcc} ({task2_map[gcc]})'] += 1 # add tweeter count to gcc tally
+        gcc_stats[gcc] += 1 # add tweeter count to gcc tally
 
 
 
@@ -127,9 +124,7 @@ with open('./data/sal.json', 'r', encoding = 'utf-8') as f:
 
 # create a list contains all greater capital cities in Australia 
 au_gcc = ['1gsyd', '2gmel', '3gbri', '4gade', '5gper', '6ghob', '7gdar', '8acte', '9othe']
-task2_map = {'1gsyd': 'Greater Sydney', '2gmel': 'Greater Melbourne', '3gbri': 'Greater Brisbane', '4gade': 'Greater Adelaide', 
-             '5gper': 'Greater Perth', '6ghob': 'Greater Hobart', '7gdar': 'Greater Darwin', '8acte': 'Greater Canberra', 
-             '9othe': 'Great Other Territories'}
+
 
 gcc_sa1 = {key:sa1[key] for key in sa1 if sa1[key]['gcc'] in au_gcc}
 
@@ -148,9 +143,9 @@ MAP_TweetState_TO_DupSALDict = {
     'Australian Capital Territory': 'act'
 }
 
-# A dictionary stores the tweet count, tweet count from each gcc and diff gccs count for each author id. It will have the following format:
-# user_stats = {'author_id_1': [tweet_count, {'gcc1': count_in_gcc1, 'gcc2': count_gcc2,...}, diff_gccs_count],
-#               'author_id_2': [...],...}
+# A dictionary stores the tweet count, tweet count from each gcc for each author id. It will have the following format:
+# user_stats = {'author_id_1': {'gcc1': count_in_gcc1, 'gcc2': count_gcc2,...},
+#               'author_id_2': {...}}
 user_stats = dict()
 
 # A dictionary stores the number of tweets made in each Greater Capital cities of Australia.
@@ -186,8 +181,8 @@ with open('./data/twitter-data-small.json', 'r', encoding = 'utf-8') as f:
             elif line in ["  },\n", "  }\n"]:
                 # update the index of the tweet
                 tweet_index += 1
-                # this process will only process tweet with tweet_index%size == rank, and author_id and tweet_location both exist
-                if tweet_index % size == rank & author_id & tweet_location:
+                # this process will only process tweet with tweet_index%size == rank
+                if tweet_index % size == rank:
                     # analyse this tweet and update stats 
                     update_stats(tweet_location, author_id)
                 # reset tweet location and author id
@@ -197,46 +192,77 @@ with open('./data/twitter-data-small.json', 'r', encoding = 'utf-8') as f:
         else:
             break
 
-#TODO: only if node = 0? 
 gcc_stats_list = comm.gather(gcc_stats, root = 0)
 user_stats_list = comm.gather(user_stats, root = 0)
-# TODO: isn't user_stats_list supposed to be a dictionary?
 
-# output for task 1
+
+# only master combines information gatherred from slaves and output the result.
 if rank == 0:
-    # QUESTION: this should still be a dictionary right?
-    task1 = [(key, user_stats_list[key][0]) for key in user_stats_list]
+    # a temporary dictionary to combine user_stats from gathered user_stats_list from each processor
+    # it has the form of : temp_user_stats = {'author_id_1': {'gcc1': count_in_gcc1, 'gcc2': count_gcc2,...},...}
+    temp_user_stats = {}
+    for d in user_stats_list:
+        for author_id, stat in d.items():
+            if author_id not in temp_user_stats:
+                temp_user_stats[author_id] = stat
+            else:
+                temp_user_stats[author_id] = Counter(temp_user_stats[author_id]) + Counter(stat)
+
+    # A dictionary stores the tweet count, tweet count from each gcc and diff gccs count for each author id. It will have the following format:
+    # user_stats = {'author_id_1': [tweet_count, {'gcc1': count_in_gcc1, 'gcc2': count_gcc2,...}, diff_gccs_count],
+    #               'author_id_2': [...],...}
+    user_stats = {}
+    # calculate number of tweet made and number of different gccs for each author
+    for author_id, stat in temp_user_stats.items():
+        # total number of tweet = sum of number of tweet made in each gcc
+        numTweet = sum(stat.values())
+        # number of distinct gcc = the number of keys in stat
+        numGcc = len(stat.keys())
+        user_stats[author_id] = [numTweet, stat, numGcc]
+
+    # get output for task 1
+    task1 = [(author, stat[0]) for author, stat in user_stats.items()]
     task1.sort(key=lambda x:x[1], reverse = True)
     task1 = task1[:10]
-
+    # get author and numTweet as two separated lists
     author_id = [obj[0] for obj in task1]
     number_of_tweets = [obj[1] for obj in task1]
     rank = [f'#{i}' for i in range(1, 11)]
-    result_task = pd.DataFrame({'Rank': rank, 'Author Id': author_id, 'Number of Tweets Made': number_of_tweets})
-    print(result_task)
-    # get runtime, TODO: delete when using spartan. 
-    print(time.time()-time_start)
+    result_task1 = pd.DataFrame({'Rank': rank, 'Author Id': author_id, 'Number of Tweets Made': number_of_tweets})
+    print(result_task1.to_string(index=False))
 
-# output for task 2
-# TODO: Question: do we need to sort?
-if rank == 0:
-    gcc_stats = Counter()
+    # get output for task 2
+    temp_gcc_stats = Counter()
     for result in gcc_stats_list:
-        gcc_stats += Counter(result)
+        temp_gcc_stats += Counter(result)
+    
+    # add the full name of gcc
+    task2_map = {'1gsyd': 'Greater Sydney', '2gmel': 'Greater Melbourne', '3gbri': 'Greater Brisbane', '4gade': 'Greater Adelaide', 
+             '5gper': 'Greater Perth', '6ghob': 'Greater Hobart', '7gdar': 'Greater Darwin', '8acte': 'Greater Canberra', 
+             '9othe': 'Great Other Territories'}
+    gcc_stats = {}
+    for gcc, value in temp_gcc_stats.items():
+        full_name = task2_map[gcc]
+        new_key = "{gcc_short} ({gcc_full})".format(gcc_short=gcc, gcc_full = full_name)
+        gcc_stats[new_key] = temp_gcc_stats[gcc]
+
     task2 = list(gcc_stats.items())    
     result_task2 = pd.DataFrame(task2, columns = ['Greater Capital City', 'Numbers of Tweets Made'])
-    print(result_task2)
-    # get runtime, TODO: delete when using spartan. 
-    print(time.time()-time_start)
+    # print the output ignoring index
+    print(result_task2.to_string(index=False))
 
 
-# task3 = sorted(list(user_stats.items()), key = lambda x:x[1][2], reverse=True)
-if rank == 0:
-    task3 = [(key, user_stats[key][0], user_stats[key][1], user_stats[key][2]) for key in user_stats]
-    task3.sort(key = lambda x:x[3], reverse = True)
+
+    # get output for task 3
+    task3 = [(author, user_stats[author][0], user_stats[author][1], user_stats[author][2]) for author in user_stats]
+    # sorted by numberOfUniqueCity, in case of a tie (numUniqueCity same), these should be ranked by numTweet.
+    task3.sort(key = lambda x:(x[3],x[1]), reverse = True)
     task3 = task3[:10]
 
     author_id = [obj[0] for obj in task1]
     number_of_city_locations_and_tweets = [get_number_of_city_locations_and_tweets(obj) for obj in task3]
-    result_task = pd.DataFrame({'Rank': rank, 'Author Id': author_id, 'Number of Unique City Locations and #Tweets': number_of_city_locations_and_tweets})
-    print(result_task)
+    result_task3 = pd.DataFrame({'Rank': rank, 'Author Id': author_id, 'Number of Unique City Locations and #Tweets': number_of_city_locations_and_tweets})
+    print(result_task3.to_string(index=False))
+
+    # get runtime, TODO: delete when using spartan. 
+    print(time.time()-time_start)

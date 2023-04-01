aec7054f063699bbf2605d108f0c8343c125e10e
Lang (Ron) Chen <Ronstuckathome@gmail.com>
2023-04-02 01:58:50 +1100

    added some comments

--- notebook/soln.py
+++ notebook/soln.py
@@ -7,16 +7,18 @@ from collections import defaultdict as dd
 from mpi4py import MPI
 from collections import Counter
 
+
+
 # BEGINNING OF FUNCTIONS
-def get_duplicate_and_non_duplicate_sal_dicts(gcc_sa1):
-    """ Get dictionaries to match ambiguous sals """
+def get_duplicate_and_non_duplicate_sal_dicts(gcc_sal):
+    """ Solving the ambiguous dictionary problem: get dictionaries to match ambiguous sals """
 
     # build up the special dictionaries used for matching ambiguous SA1s
     dup_sal_dict = {}
     non_dup_sal_dict = {}
     dup_sal_map_dict = {}
 
-    for sal in gcc_sa1:
+    for sal in gcc_sal:
         # for a duplicate suburb, there will be brackets indicating the state
         if "(" in sal:
             dic = {}
@@ -41,22 +43,21 @@ def get_duplicate_and_non_duplicate_sal_dicts(gcc_sa1):
             else:
                 dup_sal_map_dict[suburb]=dic
             
-            dup_sal_dict[sal] = gcc_sa1[sal]
+            dup_sal_dict[sal] = gcc_sal[sal]
             
         else:
-            non_dup_sal_dict[sal] = gcc_sa1[sal]
+            non_dup_sal_dict[sal] = gcc_sal[sal]
 
     return dup_sal_dict, non_dup_sal_dict, dup_sal_map_dict
 
 
 
-# TODO: change twitter_data
 def update_stats(tweet_location, author_id):
-    
+    """ Helper function to update stats given a tweet we just read """
+
     # try to just use the sa1 (e.g. try to use 'central coast' if location is 'central coast, new south wales')
     tweet_sal = tweet_location.split(',')[0] 
     
-
     # if tweet_sal is a duplicated suburb name which are repeated in differnt states:
     if tweet_sal in dup_sal_dict: 
         # extract the state from "tweet_location" as well
@@ -71,8 +72,6 @@ def update_stats(tweet_location, author_id):
             gcc = non_dup_sal_dict[tweet_sal]['gcc'] 
         # if not, then just ignore
         except:
-            # print ignored instances
-            # print('flushed out:', tweet_location) 
             return 
         
 
@@ -84,16 +83,19 @@ def update_stats(tweet_location, author_id):
             user_stats[author_id][gcc] = 1 # add gcc to tally and record 1 tweet
         else:
             user_stats[author_id][gcc] += 1 # add tweet count to gcc tally
+    # due to parallelisation, don't need to count up distinct number of locations that tweets were made in just yet.
+    # this should be done on node 0
 
     # update gcc_stats
     if gcc not in gcc_stats:
         gcc_stats[gcc] = 1 # add gcc to tally
     else:
-        gcc_stats[gcc] += 1 # add tweeter count to gcc tally
+        gcc_stats[gcc] += 1 # add tweet count to gcc tally
 
 
 
 def get_number_of_city_locations_and_tweets(obj):
+    """ Helper to get the correct printed output for task 3 """
     out = str()
     out += str(obj[3])
     out += ' (#'
@@ -121,21 +123,20 @@ def get_number_of_city_locations_and_tweets(obj):
 
 
 
-
+## main
 # read the sal file
 with open('./data/sal.json', 'r', encoding = 'utf-8') as f:
-    sa1 = json.load(f)
+    sal = json.load(f)
 
-# create a list contains all greater capital cities in Australia 
+# hardcode the list contains all greater capital cities in Australia 
 au_gcc = ['1gsyd', '2gmel', '3gbri', '4gade', '5gper', '6ghob', '7gdar', '8acte', '9othe']
 
+gcc_sal = {key:sal[key] for key in sal if sal[key]['gcc'] in au_gcc}
 
-gcc_sa1 = {key:sa1[key] for key in sa1 if sa1[key]['gcc'] in au_gcc}
+# get the duplicate and non duplicated list and mapping dictionaries using the sa1 json readin
+dup_sal_dict, non_dup_sal_dict, dup_sal_map_dict = get_duplicate_and_non_duplicate_sal_dicts(gcc_sal)
 
-
-dup_sal_dict, non_dup_sal_dict, dup_sal_map_dict = get_duplicate_and_non_duplicate_sal_dicts(gcc_sa1)
-
-# Dictionary mapping twitter data's state back to Duplicated_sal_dict
+# Hardcode dictionary mapping twitter data's state back to Duplicated_sal_dict
 MAP_TweetState_TO_DupSALDict = {
     'Tasmania': 'tas',
     'Western Australia': 'wa',
@@ -155,12 +156,14 @@ user_stats = dict()
 # A dictionary stores the number of tweets made in each Greater Capital cities of Australia.
 gcc_stats = dict()
 
+# As each individual node will read this script from start to end, they need to know which node they are within this cluster
 # get rank and size from MPI for this process
 comm = MPI.COMM_WORLD
 rank = comm.Get_rank()
 size = comm.Get_size()
-# get runtime, delete when using spartan. 
-time_start = time.time()
+
+# # get runtime, delete when using spartan. 
+# time_start = time.time()
 
 # read the twitter data by readline() to avoid running out of memory
 with open('./data/twitter-data-small.json', 'r', encoding = 'utf-8') as f:
@@ -168,9 +171,8 @@ with open('./data/twitter-data-small.json', 'r', encoding = 'utf-8') as f:
     author_id = ""
     # the first line of json file is a opening square bracket "[", we skip this line 
     next(f)
-    tweet_index = 0
     
-    tweet_index = 0
+    tweet_index = 0 
     while True:
         line = f.readline()
         # if not end of file
@@ -180,13 +182,14 @@ with open('./data/twitter-data-small.json', 'r', encoding = 'utf-8') as f:
                 # update the index of the tweet
                 tweet_index += 1
                 # this process will only process tweet with tweet_index%size == rank
-                if tweet_index % size == rank: # TODO: 为什么不check他的tweet location和author id了？？
+                if tweet_index % size == rank: 
                     # analyse this tweet and update stats 
                     update_stats(tweet_location, author_id)
                 # reset tweet location and author id
                 tweet_location = ""
                 author_id = ""
 
+            # if the tweet_index shows it is not this node's job to process this tweet, then we just skip over it.
             elif tweet_index % size != rank: 
                 continue
 
@@ -199,20 +202,21 @@ with open('./data/twitter-data-small.json', 'r', encoding = 'utf-8') as f:
             # if reached the end of a tweet:
             # "  },\n" is the ending for all tweets, excluding the last tweet and the last one has a ending of "  }\n"
             
-        # line is false: end of file
+        # line is false: end of file; so break loop
         else:
             break
 
+# if node number is root = 0, then it (also) collects the gcc_stat and user_stat dicts (in the form of list of dicts (which happen to be type of sent variables);
+# else: it sends this dictionary object back to the main
 gcc_stats_list = comm.gather(gcc_stats, root = 0)
 user_stats_list = comm.gather(user_stats, root = 0)
-print(gcc_stats_list)
 
 # only master combines information gatherred from slaves and output the result.
 if rank == 0:
-    # a temporary dictionary to combine user_stats from gathered user_stats_list from each processor
+    # use a temporary dictionary to combine user_stats from gathered user_stats_list from each processor
     # it has the form of : temp_user_stats = {'author_id_1': {'gcc1': count_in_gcc1, 'gcc2': count_gcc2,...},...}
     temp_user_stats = {}
-    for d in user_stats_list:
+    for d in user_stats_list: # for each processor's outcome
         for author_id, stat in d.items():
             if author_id not in temp_user_stats:
                 temp_user_stats[author_id] = stat
@@ -223,13 +227,14 @@ if rank == 0:
     # user_stats = {'author_id_1': [tweet_count, {'gcc1': count_in_gcc1, 'gcc2': count_gcc2,...}, diff_gccs_count],
     #               'author_id_2': [...],...}
     user_stats = {}
-    # calculate number of tweet made and number of different gccs for each author
+    # process temp_user_stats to also derive number of tweet tweeted and number of different gccs tweeted from for each author
     for author_id, stat in temp_user_stats.items():
         # total number of tweet = sum of number of tweet made in each gcc
         numTweet = sum(stat.values())
         # number of distinct gcc = the number of keys in stat
         numGcc = len(stat.keys())
         user_stats[author_id] = [numTweet, stat, numGcc]
+
 
     # get output for task 1
     task1 = [(author, stat[0]) for author, stat in user_stats.items()]
@@ -241,6 +246,7 @@ if rank == 0:
     rank = [f'#{i}' for i in range(1, 11)]
     result_task1 = pd.DataFrame({'Rank': rank, 'Author Id': author_id, 'Number of Tweets Made': number_of_tweets})
     print(result_task1.to_string(index=False))
+
 
     # get output for task 2
     temp_gcc_stats = Counter()
@@ -266,7 +272,6 @@ if rank == 0:
     print(result_task2.to_string(index=False))
 
 
-
     # get output for task 3
     task3 = [(author, user_stats[author][0], user_stats[author][1], user_stats[author][2]) for author in user_stats]
     # sorted by numberOfUniqueCity, in case of a tie (numUniqueCity same), these should be ranked by numTweet.
@@ -278,5 +283,5 @@ if rank == 0:
     result_task3 = pd.DataFrame({'Rank': rank, 'Author Id': author_id, 'Number of Unique City Locations and #Tweets': number_of_city_locations_and_tweets})
     print(result_task3.to_string(index=False))
 
-    # get runtime, TODO: delete when using spartan. 
-    print(time.time()-time_start)
+    # # get runtime, TODO: delete when using spartan. 
+    # print(time.time()-time_start)
